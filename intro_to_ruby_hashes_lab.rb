5ff0e977a51417868bf62da71443a8f81953230c
Carl Parm <carlparm@gmail.com>
2020-01-16 15:50:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,10 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  badge = {
+    id: 6
+  }
+  return badge
 end
 
 def my_hash_creator(key, value)
